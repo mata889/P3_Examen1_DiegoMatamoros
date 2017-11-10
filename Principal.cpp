b68b7a1bdf1100170d1b6e1b9e6d1248373e73ca
DiegoMatamoros <dam_900@unitec.edu>
2017-11-10 16:52:20 -0600

    arreglando matricez

--- Principal.cpp
+++ Principal.cpp
@@ -4,6 +4,8 @@
 
 Piezas*** crearTablero();
 void eliminarTablero(Piezas***);
+Piezas*** llenarTablero(Piezas***);
+void imprimirTablero(Piezas*** );
 
 using namespace std;
 
@@ -21,10 +23,27 @@ int main(){
 	cin>>j2;
 	jugador2=j2;
 
-	cout<<jugador1<<" "<<"VS"<<" "<<jugador2<<endl;
+
 
 	Piezas*** tablero=crearTablero();
-	cout<<"se ha creado el tablero"<<endl;
+//	cout<<"se ha creado el tablero"<<endl;
+	llenarTablero(tablero);
+	imprimirTablero(tablero);
+	cout<<jugador1<<" "<<"VS"<<" "<<jugador2<<endl;
+	bool win=true;
+	int cantidad=0;
+	while (win==true) {
+		if (cantidad%2==0) {
+			cout<<jugador1<<endl;
+			cantidad++;
+		}else if(cantidad%2!=0){
+			cout<<jugador2<<endl;
+			cantidad++;
+			win=false;
+		}
+	}
+
+
 
 	eliminarTablero(tablero);
 	return 0;
@@ -52,5 +71,48 @@ void eliminarTablero(Piezas*** tablero){
 }
 
 Piezas*** llenarTablero(Piezas***tabla){
-	
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if ((i==0 && (j==1 || j==3 || j==5 || j==7)) ||((i==1 &&(j==0 || j==2 || j==4 || j==6))) ||(i==2 && (j==1 || j==3 || j==5 || j==7))) {
+				tabla[i][j]=new Piezas('N','M');
+			}
+			else if((i==5 && (j==0 || j==2 || j==4 || j==6)) ||((i==7 &&(j==0 || j==2 || j==4 || j==6))) ||(i==6 && (j==1 || j==3 || j==5 || j==7))){
+				tabla[i][j]=new Piezas('B','M');
+			}else{
+				tabla[i][j]=new Piezas(' ',' ');
+			}
+		}
+	}
+	return tabla;
+}
+
+void imprimirTablero(Piezas*** tabla){
+	string letras="abcdefgh";
+	string numeros="01234567";
+	cout<<" ";
+	for (size_t i = 0; i < 8; i++) {
+		cout<<"  "<<letras.at(i)<<"  ";
+	}
+	cout<<endl;
+	for (int i = 0; i < 8; i++) {
+		cout<<numeros.at(i);
+		for (int j = 0; j < 8; j++) {
+
+			if (tabla[i][j]) {
+				cout<<"[ "<<tabla[i][j]->getColor()<<" ]";
+			}else{
+				cout<<"[ ]";
+			}
+
+		}
+
+		cout<<" "<<endl;
+	}
+}
+
+int letra(char x){
+	int numero;
+	if (x=='a') {
+		numero=0;
+	}
 }
