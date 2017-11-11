b397ad55a178b145da92cba5db3713bab92252dd
DiegoMatamoros <dam_900@unitec.edu>
2017-11-10 18:11:22 -0600

    movimiento

--- Principal.cpp
+++ Principal.cpp
@@ -6,6 +6,7 @@ Piezas*** crearTablero();
 void eliminarTablero(Piezas***);
 Piezas*** llenarTablero(Piezas***);
 void imprimirTablero(Piezas*** );
+int letra(char );
 
 using namespace std;
 
@@ -28,18 +29,67 @@ int main(){
 	Piezas*** tablero=crearTablero();
 //	cout<<"se ha creado el tablero"<<endl;
 	llenarTablero(tablero);
-	imprimirTablero(tablero);
+
 	cout<<jugador1<<" "<<"VS"<<" "<<jugador2<<endl;
 	bool win=true;
 	int cantidad=0;
 	while (win==true) {
 		if (cantidad%2==0) {
-			cout<<jugador1<<endl;
-			cantidad++;
+
+			cout<<"ES SU TURNO NEGRO: "<<jugador1<<endl;
+			string inicial,fin;
+			char x,y,xx,yy;
+			int x1,y1,xx1,yy1;
+			do {
+
+				imprimirTablero(tablero);
+				cout<<"Introduzca Posicion Inicial ej.F4"<<endl;
+				cin>>inicial;
+				x=inicial.at(0);
+				x1=letra(x);//conviertiendo la letra x a numeros
+
+				y=inicial.at(1);
+				y1=y-48;//conviertiendo la letra y a numeors
+				//cout<<y1<<"cheq";
+
+				cout<<"Introduzca Posicion fin ej.F4"<<endl;
+				cin>>fin;
+				xx=inicial.at(0);
+				xx1=letra(xx);
+
+				yy=inicial.at(1);
+				yy1=yy-48;
+				cantidad++;
+			} while((x1<0 || x1>8 )||(xx1<0 || xx1>8) || (y1<0||  y1>8 )||( yy1<0 ||yy1>8));
+			//cout<<"INTRODUJO MAL UN NUMERO,perdio su turno"<<endl;
+
+
 		}else if(cantidad%2!=0){
-			cout<<jugador2<<endl;
-			cantidad++;
-			win=false;
+			cout<<"ES SU TURNO BLANCO: "<<jugador2<<endl;
+			string inicial,fin;
+			char x,y,xx,yy;
+			int x1,y1,xx1,yy1;
+			do {
+
+				imprimirTablero(tablero);
+				cout<<"Introduzca Posicion Inicial ej.F4"<<endl;
+				cin>>inicial;
+				x=inicial.at(0);
+				x1=letra(x);//conviertiendo la letra x a numeros
+
+				y=inicial.at(1);
+				y1=y-48;//conviertiendo la letra y a numeors
+				//cout<<y1<<"cheq";
+
+				cout<<"Introduzca Posicion fin ej.F4"<<endl;
+				cin>>fin;
+				xx=inicial.at(0);
+				xx1=letra(xx);
+
+				yy=inicial.at(1);
+				yy1=yy-48;
+				cantidad++;
+			} while((x1<0 ||  x1>8 )|| (xx1<0 ||  xx1>8) ||  (y1<0 ||  y1>8 )|| ( yy1<0 ||  yy1>8));
 		}
 	}
 
@@ -114,5 +164,20 @@ int letra(char x){
 	int numero;
 	if (x=='a') {
 		numero=0;
+	}else if (x=='b') {
+		numero=1;
+	}else if (x=='c') {
+		numero=2;
+	}else if (x=='d') {
+		numero=3;
+	}else if (x=='e') {
+		numero=4;
+	}else if (x=='f') {
+		numero=5;
+	}else if (x=='g') {
+		numero=6;
+	}else if (x=='h') {
+		numero=7;
 	}
+	return  numero;
 }
